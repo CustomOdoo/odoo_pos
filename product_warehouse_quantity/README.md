f405e3703e1095cc28f9b12e02e969bed1c18cf3
athmanziri <issaziri@gmail.com>
2018-09-22 12:12:07 +0300

    added product warehouse quantity

---
+++ product_warehouse_quantity/README.md
@@ -0,0 +1,8 @@
+Quick access of quantity available in each warehouse
+----------------------------------------------------
+
+This module is a step forward to allow the user to have access to analysed information quickly.
+This module helps specifically for multi warehouse structure, to find the number of quantity available in each of the warehouse.
+The warehoue has the one stock location.
+There may be various child location of each of that stok location. Each of such location may have some quantity of the product.
+This module shows the cumulative sum of total quantity available in each varehosue and shows on the product form view and kanban view.
---
+++ product_warehouse_quantity/models/product.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2017 Ascetic Business Solution <www.asceticbs.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
+from odoo import api, fields, models, _
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    warehouse_quantity = fields.Char(compute='_get_warehouse_quantity', string='Quantity per warehouse')
+
+    def _get_warehouse_quantity(self):
+        for record in self:
+            warehouse_quantity_text = ''
+            product_id = self.env['product.product'].sudo().search([('product_tmpl_id', '=', record.id)])
+            if product_id:
+                quant_ids = self.env['stock.quant'].sudo().search([('product_id','=',product_id[0].id),('location_id.usage','=','internal')])
+                t_warehouses = {}
+                for quant in quant_ids:
+                    if quant.location_id:
+                        if quant.location_id not in t_warehouses:
+                            t_warehouses.update({quant.location_id:0})
+                        t_warehouses[quant.location_id] += quant.quantity
+
+                tt_warehouses = {}
+                for location in t_warehouses:
+                    warehouse = False
+                    location1 = location
+                    while (not warehouse and location1):
+                        warehouse_id = self.env['stock.warehouse'].sudo().search([('lot_stock_id','=',location1.id)])
+                        if len(warehouse_id) > 0:
+                            warehouse = True
+                        else:
+                            warehouse = False
+                        location1 = location1.location_id
+                    if warehouse_id:
+                        if warehouse_id.name not in tt_warehouses:
+                            tt_warehouses.update({warehouse_id.name:0})
+                        tt_warehouses[warehouse_id.name] += t_warehouses[location]
+
+                for item in tt_warehouses:
+                    if tt_warehouses[item] != 0:
+                        warehouse_quantity_text = warehouse_quantity_text + ' ** ' + item + ': ' + str(tt_warehouses[item])
+                record.warehouse_quantity = warehouse_quantity_text
