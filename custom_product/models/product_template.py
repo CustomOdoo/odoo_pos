aad02b3779094d9152dd7c0e31c232b2ae867be7
athmanziri <issaziri@gmail.com>
2018-11-06 09:22:43 +0300

    modified custom product

--- custom_product/models/product_template.py
+++ custom_product/models/product_template.py
@@ -15,12 +15,16 @@ class CustomProductTag(models.Model):
 class CustomProductTemplate(models.Model):
 	_inherit = 'product.template'
 
+	def _get_default_uom_id(self):
+		return self.env["product.uom"].search([], limit=1, order='id').id
+
 	product_tag = fields.Many2one('custom.product.tag',string='Local / Dynamix')
 	packing_size = fields.Char('Packing Size')
-	# requisition_price = fields.Float('Requisition Price', 
-	# 							digits=dp.get_precision('Product Price'), 
-	# 							help="Requisition price.")
-	list_price = fields.Float(string="Wholesale Price")
-	uom_id = fields.Many2one(string="Units/Case for sale")
-	uom_po_id = fields.Many2one(string="Units/Case")
+	# list_price = fields.Float(string="Wholesale Price")
+	# uom_id = fields.Many2one(string="Units/Case for sale")
+	# uom_po_id = fields.Many2one(string="Units/Case")
 	# type = fields.Selection(default='product')
+	uom_pos_id = fields.Many2one(
+        'product.uom', 'Unit of Measure for POS',
+        default=_get_default_uom_id, required=True,
+        help="Default Unit of Measure used for all stock operation.")
