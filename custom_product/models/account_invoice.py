00b890122c43af31c2b17ed7832a7fab1b59be0d
athmanziri <issaziri@gmail.com>
2019-02-21 10:52:52 +0300

    modified invoice

--- custom_product/models/account_invoice.py
+++ custom_product/models/account_invoice.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api, _
 class AcountInvoice(models.Model):
 	_inherit = 'account.invoice'
 
-	packing_size = fields.Char(related='invoice_line_id.product_id.packing_size')
+	packing_size = fields.Char(related='invoice_line_ids.product_id.packing_size')
